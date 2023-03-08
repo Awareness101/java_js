038c6f38dda9f6d44fecd09a0cbbff642e5922a3
MacBookpro <2085095547@qq.com>
2023-03-08 10:22:45 +0800

    three test

--- src/handwrittencode/Account.java
+++ src/handwrittencode/Account.java
@@ -3,6 +3,7 @@ package handwrittencode;
 // test rebase the current branch on top of incoming change
 
 //commit...
+//commit... retest
 public  abstract class Account {
     public abstract void  withdraw();
 }
